180fb3100c2c34fe9940844ae1c6d0f439069bb6
Monica Namagembe <namagembe.monicatendo@students.mak.ac.ug>
2026-09-09 16:55:36 +0300

    Assignment

--- Chapter_02/population.cpp
+++ Chapter_02/population.cpp
@@ -6,3 +6,25 @@
 // Write a program to display the population for each of the next five years. Assume the current population is 312032486 and one year has 365 days.
 
 
+#include <iostream>
+int main() {
+    int population = 312032486;
+    const int DAYS_IN_YEAR = 365;
+    const int SECONDS_IN_YEAR = DAYS_IN_YEAR * 24 * 60 * 60;
+    const int BIRTH_RATE = 7; 
+    const int DEATH_RATE = 13;
+    const int IMMIGRANT_RATE = 45;
+    int births_per_year = SECONDS_IN_YEAR / BIRTH_RATE;
+    int deaths_per_year = SECONDS_IN_YEAR / DEATH_RATE;
+    int immigrants_per_year = SECONDS_IN_YEAR / IMMIGRANT_RATE;
+    int net_popoulation_change_per_year = births_per_year - deaths_per_year + immigrants_per_year;
+    std::cout << "Projected population for the next 5 years:\n";
+    std::cout <<"-------------------------------------------\n";
+    for (int year = 1; year <= 5; ++year){
+        population += net_popoulation_change_per_year;
+        std::cout << "Year " << year << ": " <<population << "\n";
+    }
+    return 0;
+
+
+}
--- Chapter_02/triangle_area.cpp
+++ Chapter_02/triangle_area.cpp
@@ -4,3 +4,20 @@
 // Area = sqrt(s(s-side1)(s-side2)(s-side3))
 // Example: Enter three points for a triangle: 1.5, -3.4, 4.6, 5, 9.5, -3.4 
 // The area of the triangle is 33.6
+#include <iostream>
+#include <cmath> 
+int main() {
+    double a, b, c;
+    std::cout <<"Enter three sides: ";
+    std::cin >> a >> b >> c;
+    if(a + b > c && a + c > b && b + c > a) {
+        double s = (a + b + c) / 2.0;
+        double area = std::sqrt(s * (s - a) * (s - b) * (s - c));
+        std::cout << "The area is: " << area << std::endl;
+
+    }
+    else{
+        std::cout << "Invalid triangle sides!" << std::endl;
+    }
+    return 0;
+}
